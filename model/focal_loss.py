df6686c0a90b14af541d3ad87d3d1da280259f92
Xiaojun Hu <xiahu@malong.com>
2019-07-30 17:10:09 +0800

    replace == by tf.equal

--- model/focal_loss.py
+++ model/focal_loss.py
@@ -1,18 +1,27 @@
 import tensorflow as tf
 
-def focal_loss_sigmoid(target_tensor, prediction_tensor, alpha=0.25, gamma=2):
-    per_entry_cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(
-        labels=target_tensor, logits=prediction_tensor)
-    prediction_probabilities = tf.sigmoid(prediction_tensor)
-    p_t = ((target_tensor * prediction_probabilities) +
-           ((1 - target_tensor) * (1 - prediction_probabilities)))
-    modulating_factor = tf.pow(1.0 - p_t, gamma)
-    alpha_weight_factor = (target_tensor * alpha + (1 - target_tensor) * (1 - alpha))
-    focal_cross_entropy_loss = (modulating_factor * alpha_weight_factor * per_entry_cross_ent)
-    focal_cross_entropy_loss = tf.reduce_sum(focal_cross_entropy_loss)
-    return focal_cross_entropy_loss
+def focal_loss_sigmoid(pred,
+                       target,
+                       gamma=2.0,
+                       alpha=0.25,
+                       weight=None):
+    # predict probability
+    pred_sigmoid = tf.sigmoid(pred)
 
+    # focal weight
+    pt = tf.where(
+        tf.equal(target, 1.0),
+        1.0 - pred_sigmoid,
+        pred_sigmoid)
+    alpha_weight = (alpha * target) + ((1 - alpha) * (1 - target)) 
+    focal_weight = alpha_weight * tf.pow(pt, gamma)
+    if weight is not None:
+        focal_weight = focal_weight*weight
 
-def focal_loss_with_reduce(target_tensor, prediction_tensor):
-    loss = focal_loss_sigmoid(target_tensor, prediction_tensor)
-    return tf.reduce_sum(loss, axis=(0,1,2)) / (tf.reduce_sum(target_tensor, axis=(0,1,2))+1)
+    # loss
+    bce = tf.nn.sigmoid_cross_entropy_with_logits(labels=target, logits=pred)
+    loss = bce * focal_weight
+    loss = tf.reduce_sum(loss)
+    
+    return loss
+
