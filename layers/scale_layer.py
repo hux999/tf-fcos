5cac870c2087ed172e9df5046f07c804e78bd416
Xiaojun Hu <xiahu@malong.com>
2019-07-30 17:00:35 +0800

    add init_value

--- layers/scale_layer.py
+++ layers/scale_layer.py
@@ -4,12 +4,16 @@ from tensorflow.keras.layers import Layer, InputSpec
 
 class ScaleLayer(Layer):
 
-    def __init__(self, **kwargs):
+    def __init__(self, init_value=1.0, **kwargs):
         super(ScaleLayer, self).__init__(**kwargs)
+        self.init_value = init_value
 
     def build(self, input_shape):
         shape = [1] * len(input_shape)
-        self.scale = self.add_weight(shape=shape, name='scale')
+        self.scale = self.add_weight(
+            shape=shape,
+            initializer=tf.keras.initializers.Constant(self.init_value),
+            name='scale')
 
     def call(self, inputs):
         outputs = self.scale * inputs
